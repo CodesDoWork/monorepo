38db2c8fb651a34cab0f77d8970afc1751a8b0ba
Justin Konratt <codesdowork@gmail.com>
2023-06-12 11:40:35 +0200

    feat(shared/trpc): improved typings

--- libs/shared/trpc/src/lib/createAppRouter.ts
+++ libs/shared/trpc/src/lib/createAppRouter.ts
@@ -2,4 +2,5 @@ import { AnyRouter } from "@trpc/server";
 import { mergeRouters } from "./trpc";
 import { healthRouter } from "./procedures/health";
 
-export const createAppRouter = (...routers: AnyRouter[]) => mergeRouters(...routers, healthRouter);
+export const createAppRouter = <TRouters extends AnyRouter[]>(...routers: TRouters) =>
+    mergeRouters(...routers, healthRouter);

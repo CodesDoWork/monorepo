085994597643cc284b5e0f5850f08e5b5b0531ea
Justin Konratt <codesdowork@gmail.com>
2023-07-21 19:17:45 +0200

    feat(scoreboard): added nextjs scoreboard app to server

---
+++ apps/server/scoreboard/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Index() {
+    return (
+        <div className="wrapper">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
+}

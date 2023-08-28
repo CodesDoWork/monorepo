8ad9605f65a44b6d5a6913adc8a89c81a68d7e14
Justin Konratt <codesdowork@gmail.com>
2023-08-28 16:26:27 +0200

    feat(server-nutrition): remove nutrition service

--- libs/server/nutrition/extensions-bundle/src/fdc/api/get-nutrient.ts
+++
@@ -1,8 +0,0 @@
-import { FDCItem, FoodNutrient } from "../../types/fdc-api";
-
-export function getNutrient(item: FDCItem, name: string, unit?: string): FoodNutrient | undefined {
-    return item.foodNutrients.find(
-        nutrient =>
-            nutrient.nutrient.name === name && (!unit || nutrient.nutrient.unitName === unit),
-    );
-}

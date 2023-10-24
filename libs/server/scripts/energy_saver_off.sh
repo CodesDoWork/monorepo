e2da04c8ff56d217f8198f0dff2487002d95f2fd
Justin Konratt <codesdowork@gmail.com>
2023-10-24 14:50:41 +0200

    feat(server scripts): added energy saver scripts

---
+++ libs/server/scripts/energy_saver_off.sh
@@ -0,0 +1,6 @@
+docker compose -f /home/jkonratt/server/docker-compose.yml up -d
+
+apt-get -y autoremove
+apt-get -y update
+apt-get -y upgrade
+apt-get -y clean
---
+++ libs/server/scripts/energy_saver_on.sh
@@ -0,0 +1,14 @@
+docker stop \
+    auth \
+    backup-service \
+    dashboard \
+    db \
+    docker-registry \
+    documentation \
+    gitlab \
+    music \
+    music-downloader \
+    portainer \
+    scoreboard \
+    sonarqube \
+    verdaccio

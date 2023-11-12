66b23529220afce5d4e5fa757890cdf83024dd4e
Justin Konratt <codesdowork@gmail.com>
2023-11-12 14:56:53 +0100

    chore(server): improved energy_saver_off script

--- libs/server/scripts/energy_saver_off.sh
+++ libs/server/scripts/energy_saver_off.sh
@@ -1,5 +1,13 @@
-docker compose -f /home/jkonratt/server/docker-compose.yml pull
-docker compose -f /home/jkonratt/server/docker-compose.yml up -d
+SERVER_DIR=/home/jkonratt/server
+COMPOSE_FILE=$SERVER_DIR/docker-compose.yml
+DOCKER_LOGIN_FILE=$SERVER_DIR/docker-registry/.login
+DOCKER_REGISTRY=justin.konratts.de
+DOCKER_REGISTRY_SERVICE=docker-registry
+
+docker compose -f $COMPOSE_FILE up -d --pull always --wait $DOCKER_REGISTRY_SERVICE
+tail -1 $DOCKER_LOGIN_FILE | docker login $DOCKER_REGISTRY --username $(head -1 $DOCKER_LOGIN_FILE) --password-stdin
+docker compose -f $COMPOSE_FILE up -d --pull always
+docker compose  -f $COMPOSE_FILE exec $DOCKER_REGISTRY_SERVICE registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true
 
 apt-get -y autoremove
 apt-get -y update

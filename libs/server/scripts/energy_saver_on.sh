c0d15b4ae3a5319e89fa8c992405836bf6b04e00
Justin Konratt <codesdowork@gmail.com>
2023-12-13 18:32:43 +0100

    feat(server): add off-at-night profile and bitwarden service

--- libs/server/scripts/energy_saver_on.sh
+++ libs/server/scripts/energy_saver_on.sh
@@ -1,15 +1,1 @@
-docker compose -f /home/jkonratt/server/docker-compose.yml stop \
-    dashboard \
-    docker-registry \
-    documentation \
-    doku \
-    gitlab \
-    gitlab-runner \
-    grafana \
-    music \
-    music-downloader \
-    netdata \
-    portainer \
-    prometheus \
-    scoreboard \
-    sonarqube
+docker compose -f /home/jkonratt/server/docker-compose.yml --profile off-at-night stop

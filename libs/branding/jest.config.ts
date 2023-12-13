a1569589c337933770a0e0d05dad768f463f5f00
Justin Konratt <codesdowork@gmail.com>
2023-12-13 17:19:13 +0000

    Ci/pipeline

--- libs/branding/jest.config.ts
+++ libs/branding/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-    displayName: "shared-branding",
+    displayName: "branding",
     preset: "../../jest.preset.js",
     testEnvironment: "node",
     transform: {
         "^.+\\.[tj]sx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-    coverageDirectory: "../../coverage/libs/shared/branding",
+    coverageDirectory: "../../coverage/libs/branding",
 };

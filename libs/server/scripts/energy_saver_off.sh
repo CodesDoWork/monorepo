3de7b1d0f817ae627fe8b15ac5bc45bd1275df5e
Justin Konratt <codesdowork@gmail.com>
2023-12-14 01:23:01 +0100

    chore: added day and always profiles to docker compose

--- libs/server/scripts/energy_saver_off.sh
+++ libs/server/scripts/energy_saver_off.sh
@@ -6,8 +6,8 @@ DOCKER_REGISTRY_SERVICE=docker-registry
 
 docker compose -f $COMPOSE_FILE up -d --pull always --wait $DOCKER_REGISTRY_SERVICE
 tail -1 $DOCKER_LOGIN_FILE | docker login $DOCKER_REGISTRY --username $(head -1 $DOCKER_LOGIN_FILE) --password-stdin
-docker compose -f $COMPOSE_FILE up -d --pull always
-docker compose  -f $COMPOSE_FILE exec $DOCKER_REGISTRY_SERVICE registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true
+docker compose -f $COMPOSE_FILE --profile always --profile day up -d --pull always
+docker compose -f $COMPOSE_FILE exec $DOCKER_REGISTRY_SERVICE registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true
 
 docker image prune -af
 docker volume prune -af
--- libs/server/scripts/energy_saver_on.sh
+++ libs/server/scripts/energy_saver_on.sh
@@ -1,1 +1,1 @@
-docker compose -f /home/jkonratt/server/docker-compose.yml --profile off-at-night stop
+docker compose -f /home/jkonratt/server/docker-compose.yml --profile day stop

223117fa3e9e8911573320817058bdba4b985dda
Justin Konratt <codesdowork@gmail.com>
2024-01-20 12:56:45 +0100

    feat(server-scripts): more detailed playlists

--- libs/server/scripts/create-playlists.sh
+++ libs/server/scripts/create-playlists.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 root_dir=$1
+previous_length=0
 
-declare -A audio_files
-
+rm $root_dir/*.m3u
 while IFS= read -r -d '' file; do
     if [[ $file == *.mp3 || $file == *.wav ]]; then
+        spaces=$(printf '%*s' $((previous_length - ${#file})) "")
+        previous_length=${#file}
+        echo -ne "$file$spaces\r"
+
         subdir=$(dirname "$file")
-        subdir_path=$(echo $subdir | cut -d'/' -f2-)
-        subdir_name=$(echo $subdir | cut -d'/' -f2)
         file_path=$(echo $file | cut -d'/' -f2-)
-        if [[ ! -v audio_files["$subdir_name"] ]]; then
-            audio_files["$subdir_name"]="$root_dir/$subdir_name.m3u"
-			rm "${audio_files["$subdir_name"]}"
-            touch "${audio_files["$subdir_name"]}"
-        fi
-        echo "$file_path" >> "${audio_files["$subdir_name"]}"
+        depth=$(( $(echo "${file_path//[^\/]}" | wc -m) - 1 ))
+        for level in $(seq 1 $depth); do
+            level_subdir=$(echo $subdir | cut -d'/' -f2-$((level + 1)))
+            subdir_playlist_name=$(echo $level_subdir | tr '/' '-')
+
+            if [ "$level" -eq "$depth" ]; then
+                echo "$file_path" >> "$root_dir/$subdir_playlist_name.m3u"
+            fi
+
+            if [ $(find "$root_dir/$level_subdir" -mindepth 1 -type d | wc -l) -gt 0 ]; then
+                echo "$file_path" >> "$root_dir/$subdir_playlist_name-All.m3u"
+            fi
+        done
     fi
 done < <(find "$root_dir" -type f -print0)

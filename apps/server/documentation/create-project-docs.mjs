6a9d1c52f27e387f644c308576da136b86c4cb58
Justin Konratt <codesdowork@gmail.com>
2024-02-16 09:16:42 +0100

    fix(docs): test build command with quotes in docker

--- apps/server/documentation/create-project-docs.mjs
+++ apps/server/documentation/create-project-docs.mjs
@@ -8,12 +8,12 @@ projects.forEach(({ name, paths }) => {
     console.log(`Creating docs for ${name}`);
     const entryPoints = paths.join(" ");
     execSync(
-        `typedoc --plugin typedoc-plugin-markdown --name ${name} --exclude **/node_modules/** --tsconfig ./tsconfig.base.json --out ./apps/server/documentation/docs/projects/${name}/code-docs --entryPointStrategy expand ./src ${entryPoints}`,
+        `typedoc --plugin typedoc-plugin-markdown --name ${name} --exclude "**/node_modules/**" --tsconfig ./tsconfig.base.json --out ./apps/server/documentation/docs/projects/${name}/code-docs --entryPointStrategy expand ./src ${entryPoints}`,
     );
 
     paths.forEach(path => {
         execSync(
-            `copyfiles -a -e ${path}/node_modules/**/*.md -e ./apps/server/documentation/docs/**/*.md ${path}/**/*.md ./apps/server/documentation/docs/projects/${name}`,
+            `copyfiles -a -e "${path}/node_modules/**/*.md" -e "./apps/server/documentation/docs/**/*.md" "${path}/**/*.md" ./apps/server/documentation/docs/projects/${name}`,
         );
     });
 

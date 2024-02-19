14278ec3a3e59056fff2e1e84531c39ca6d0f989
Justin Konratt <codesdowork@gmail.com>
2024-02-19 13:29:21 +0100

    fix(server-scripts): update create_playlists.sh

--- libs/server/scripts/misc/create_playlists.sh
+++ libs/server/scripts/misc/create_playlists.sh
@@ -12,18 +12,17 @@ while IFS= read -r -d '' file; do
         echo -ne "$file$spaces\r"
 
         subdir=$(dirname "$file")
-        file_path=$(echo $file | cut -d'/' -f2-)
-        depth=$(echo "${file_path//[^\/]}" | wc -m)
+        depth=$(echo "${file//[^\/]}" | wc -m)
         for level in $(seq 1 $depth); do
             level_subdir=$(echo $subdir | cut -d'/' -f1-$((level + 1)))
             subdir_playlist_name=$(echo $level_subdir | tr '/' '-')
 
             if [ "$level" -eq "$depth" ]; then
-                echo "$file_path" >> "$root_dir/$subdir_playlist_name.m3u"
+                echo "$file" >> "$root_dir/$subdir_playlist_name.m3u"
             fi
 
             if [ $(find "$root_dir/$level_subdir" -mindepth 1 -type d | wc -l) -gt 0 ]; then
-                echo "$file_path" >> "$root_dir/$subdir_playlist_name-All.m3u"
+                echo "$file" >> "$root_dir/$subdir_playlist_name-All.m3u"
             fi
         done
     fi

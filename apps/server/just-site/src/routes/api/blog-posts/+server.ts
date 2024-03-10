a8c6935c33e421e75bc51bef396395c22dd15b24
Justin Konratt <codesdowork@gmail.com>
2024-03-10 17:06:13 +0100

    fix(server-just-site): use fetch instead of axios for ghost api

--- apps/server/just-site/src/routes/api/blog-posts/+server.ts
+++ apps/server/just-site/src/routes/api/blog-posts/+server.ts
@@ -1,16 +1,9 @@
-import GhostContentAPI from "@tryghost/content-api";
-import { env } from "../../../env";
-
-const api = new GhostContentAPI({
-    url: "https://blog.justinkonratt.com",
-    key: env.GHOST_API_KEY,
-    version: "v5.0",
-});
+import { ghostApi } from "../../../helpers/ghostApi";
 
 export async function GET({ url }) {
     const limit = url.searchParams.get("limit");
     const page = url.searchParams.get("page");
-    const posts = await api.posts.browse({ limit, page, include: ["tags", "authors"] });
+    const posts = await ghostApi.posts.browse({ limit, page, include: ["tags", "authors"] });
 
     return new Response(JSON.stringify({ posts }));
 }
--- apps/server/just-site/src/routes/blog/+page.server.ts
+++ apps/server/just-site/src/routes/blog/+page.server.ts
@@ -1,14 +1,7 @@
-import GhostContentAPI from "@tryghost/content-api";
-import { env } from "../../env";
-
-const api = new GhostContentAPI({
-    url: "https://blog.justinkonratt.com",
-    key: env.GHOST_API_KEY,
-    version: "v5.0",
-});
+import { ghostApi } from "../../helpers/ghostApi";
 
 export async function load() {
-    const featuredPosts = await api.posts.browse({
+    const featuredPosts = await ghostApi.posts.browse({
         filter: "featured:true",
         include: ["tags", "authors"],
     });

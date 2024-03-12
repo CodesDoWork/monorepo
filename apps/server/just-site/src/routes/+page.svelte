dba4f4df9ce7997180f98ed499cd12707259398c
Justin Konratt <codesdowork@gmail.com>
2024-03-12 07:22:52 +0100

    fix(server-just-site): change grid template rows on landing page with media queries

--- apps/server/just-site/src/routes/+page.svelte
+++ apps/server/just-site/src/routes/+page.svelte
@@ -35,7 +35,7 @@
             </Link>
         {/each}
     </div>
-    <div class="grid grid-cols-1 grid-rows-[repeat(3,5rem)] md:grid-cols-2 gap-4 w-4/5 sm:w-3/4 md:w-11/12 lg:w-full xl:w-3/4 mx-auto">
+    <div class="grid grid-cols-1 grid-rows-[repeat(5,5rem)] md:grid-rows-[repeat(3,5rem)] md:grid-cols-2 gap-4 w-4/5 sm:w-3/4 md:w-11/12 lg:w-full xl:w-3/4 mx-auto">
         {#each homePageLinks as navLink, idx (idx)}
             <Card style={`--hover-color: ${findRouteColor(navLink.route)}; ${animationDelay(idx)}`}
                   class="flex group border-l-4 hover:border-l-8 border-[var(--hover-color)] !outline-0 !hover:outline-0">

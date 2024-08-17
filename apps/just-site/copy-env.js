728d89852873c9cdb9a1535721ab47c4a66b3cc6
Justin Konratt <codesdowork@gmail.com>
2024-08-17 19:38:56 +0200

    fix(just-site): pre-build script

--- apps/just-site/copy-env.js
+++ apps/just-site/copy-env.js
@@ -1,8 +1,5 @@
 import { readFileSync, writeFileSync } from "node:fs";
 
-let content = readFileSync("..env.template").toString();
-
-// api key must have 26 characters (also at build time)
-content = content.replace(/(?<=GHOST_API_KEY=)\w+/, "12345678901234567890123456");
+let content = readFileSync(".env.template").toString();
 
 writeFileSync(".env", content);

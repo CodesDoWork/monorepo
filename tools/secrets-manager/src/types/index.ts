6926b13c7c67d6ea6bfb6ae709b84368d6336f35
Justin Konratt <codesdowork@gmail.com>
2024-09-10 11:44:35 +0200

    fix(secrets-manager): make schemas strict

--- tools/secrets-manager/src/types/index.ts
+++ tools/secrets-manager/src/types/index.ts
@@ -6,23 +6,29 @@ export interface Credentials {
 }
 
 export const zEnvValueTypes = z.union([z.string(), z.number(), z.boolean()]);
-export const zEnvConfig = z.object({ env: z.record(zEnvValueTypes).optional().default({}) });
+export const zEnvConfig = z
+    .object({ env: z.record(zEnvValueTypes).optional().default({}) })
+    .strict();
 
 export const zSecretEnvConfig = z.union([
     z.string(),
-    z.object({ name: z.string(), field: z.string().optional() }),
+    z.object({ name: z.string(), field: z.string().optional() }).strict(),
 ]);
-export const zSecretCollectionConfig = z.object({
-    collectionId: z.string(),
-    prefix: z.boolean().optional().default(false),
-    vars: z.array(zSecretEnvConfig),
-});
-export const zSecretsConfig = z.object({
-    secrets: z.record(zSecretCollectionConfig).optional().default({}),
-});
+export const zSecretCollectionConfig = z
+    .object({
+        collectionId: z.string(),
+        prefix: z.boolean().optional().default(false),
+        vars: z.array(zSecretEnvConfig),
+    })
+    .strict();
+export const zSecretsConfig = z
+    .object({
+        secrets: z.record(zSecretCollectionConfig).optional().default({}),
+    })
+    .strict();
 
 export const zProjectSecretsConfig = z.intersection(zEnvConfig, zSecretsConfig);
-export const zRootSecretConfig = z.object({ server: z.string().optional() });
+export const zRootSecretConfig = z.object({ server: z.string().optional() }).strict();
 
 export type SecretEnvConfig = z.infer<typeof zSecretEnvConfig>;
 export type SecretCollectionConfig = z.infer<typeof zSecretCollectionConfig>;

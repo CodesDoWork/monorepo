ff5432e45469c6404767a3ed839a6a313e634283
Justin Konratt <codesdowork@gmail.com>
2024-09-11 15:05:29 +0200

    feat(maintenance): design for mobile

--- apps/maintenance/astro.config.mjs
+++ apps/maintenance/astro.config.mjs
@@ -6,9 +6,10 @@ export default defineConfig({
     srcDir: "./src",
     vite: {
         server: {
+            host: "0.0.0.0",
             fs: {
                 allow: ["../../node_modules", "../../libs/branding/assets"],
-            },
+            }
         },
         cacheDir: "../../node_modules/.vite/apps/maintenance",
     },

289538ba280d26a4779d1f1bc5cbedc60aa02b86
Justin Konratt <codesdowork@gmail.com>
2024-09-18 13:09:15 +0200

    feat(server-scripts): no longer needed, moved all cronjobs to ofelia

--- libs/server/scripts/scripts+x-deploy-command.mjs
+++
@@ -1,8 +0,0 @@
-import { globSync } from "glob";
-
-const deployDir = "libs/server/scripts/deploy/";
-const filePaths = globSync(`${deployDir}**/*.sh`).map(
-    file => "/" + file.replace(/\\/g, "/").substring(deployDir.length),
-);
-
-console.log("chmod +x", filePaths.join(" "));

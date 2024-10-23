7f29236f2ec8a6aa32ab4c82ee9c052e5f92629d
Justin Konratt <codesdowork@gmail.com>
2024-10-23 09:18:34 +0200

    mv src to packages

---
+++ packages/just-site/src/components/Footer.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import { clsx } from "clsx";
+    import Link from "./Link.svelte";
+    import { useRoutes } from "../stores/useRoutes";
+    import type { JustSiteRoutes } from "@codesdowork/cms/types";
+
+    let className = "";
+    export { className as class };
+    export let routes: JustSiteRoutes[];
+    export let licenseType: string = "";
+    export let licenseUrl: string = "";
+    export let projectUrl: string = "";
+    export let projectPlatform: string = "";
+
+    const { currentRoute, previousRoute } = useRoutes(routes);
+
+    let footerVisibility = "";
+    $: if ($currentRoute?.is_hero) {
+        footerVisibility = "scale-0";
+    } else if ($previousRoute?.is_hero === false) {
+        footerVisibility = "scale-100";
+    } else {
+        footerVisibility = "animate-grow opacity-100 scale-100";
+    }
+
+    $: footerClass = clsx(
+        "py-4 px-8 shadow select-none",
+        "font-mono text-center",
+        "bg-black dark:bg-primary-500 bg-opacity-20 dark:bg-opacity-20 text-white transition-colors",
+        "origin-bottom",
+        footerVisibility,
+        className,
+    );
+
+    const linkClass = "!text-black dark:!text-brandOrange-500 hover:!bg-brandOrange-500 hover:!text-black";
+</script>
+
+<footer class={footerClass}>
+    &copy; Justin Konratt {new Date().getFullYear()} - Licensed under
+    <Link class={linkClass} href={licenseUrl} title="License">{licenseType}</Link>
+    - View on
+    <Link class={linkClass} href={projectUrl} title="Project">{projectPlatform}</Link>
+</footer>

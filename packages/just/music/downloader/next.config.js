81af52b78c95c2a079147d289d35a6930543a47c
Justin Konratt <codesdowork@gmail.com>
2024-11-01 13:42:47 +0100

    feat(just-music-downloader): infer tasks using @nx/next plugin

--- packages/just/music/downloader/next.config.js
+++ packages/just/music/downloader/next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
         // See: https://github.com/gregberge/svgr
         svgr: false,
     },
+    distDir: "../../../../dist/packages/just/music/downloader",
 };
 
 const plugins = [

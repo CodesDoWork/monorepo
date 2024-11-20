2a31e2592d2bd50147b9eb6ec12c077db86e3453
Justin Konratt <codesdowork@gmail.com>
2024-11-20 16:36:16 +0100

    feat(just-maintenance, zinzow-test-auth): migrated workers to ts

--- packages/just/maintenance/worker/worker.ts
+++ packages/just/maintenance/worker/worker.ts
@@ -1,14 +1,18 @@
 import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 
-addEventListener("fetch", event => {
+addEventListener("fetch", (event: FetchEvent) => {
     event.respondWith(handleRequest(event));
 });
 
-async function handleRequest(event) {
+async function handleRequest(event: FetchEvent) {
     try {
         return await getAssetFromKV(event);
     } catch (e) {
         const url = new URL(event.request.url);
+        if (url.pathname === "/") {
+            return new Response("Infinite Redirect", { status: 508 });
+        }
+
         url.pathname = "/";
         return Response.redirect(url, 302);
     }

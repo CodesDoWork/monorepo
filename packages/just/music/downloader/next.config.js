be8a884b9c1527eb5b60e4c7fadd6b60f9c152e7
Justin Konratt <codesdowork@gmail.com>
2024-11-21 19:35:51 +0100

    fix(just-music-downloader): remove unused var

--- packages/just/music/downloader/next.config.js
+++ packages/just/music/downloader/next.config.js
@@ -2,7 +2,6 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require("@nx/next");
-const path = require("node:path");
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}

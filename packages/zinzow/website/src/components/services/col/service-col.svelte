bebdb97e85b8f7e1970485ce690392bd243cdef9
Justin Konratt <codesdowork@gmail.com>
2025-02-23 16:42:20 +0100

    feat(zinzow-website): center text on service card

--- packages/zinzow/website/src/components/services/col/service-col.svelte
+++ packages/zinzow/website/src/components/services/col/service-col.svelte
@@ -17,15 +17,13 @@
                     alt={service.route.name}
                     src={service.thumbnail.route} />
                 <div
-                    class="rounded-xl px-8 py-12 transition group-hover:bg-white/75 dark:group-hover:bg-black/75">
-                    <div class="-mt-9 pb-16 transition-all group-hover:mt-0 group-hover:pb-3">
-                        <H2
-                            class="dark:text-primary-200 w-fit rounded-full bg-white/75 px-3 py-1 text-sm transition-all group-hover:bg-transparent group-hover:text-3xl sm:text-base sm:group-hover:text-4xl dark:bg-black/75">
-                            {service.route.name}
-                        </H2>
-                    </div>
+                    class="group-hover:py-18 relative rounded-xl px-4 py-16 transition group-hover:bg-white/75 dark:group-hover:bg-black/75">
+                    <H2
+                        class="dark:text-primary-200 absolute left-3 top-2 w-fit text-nowrap rounded-full bg-white/75 px-3 py-1 text-sm transition-all group-hover:left-1/2 group-hover:right-1/2 group-hover:top-12 group-hover:-translate-x-1/2 group-hover:bg-transparent group-hover:text-3xl sm:text-base sm:group-hover:text-4xl dark:bg-black/75">
+                        {service.route.name}
+                    </H2>
                     <p
-                        class="mt-4 translate-y-4 text-center text-base opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100">
+                        class="mx-2 mt-12 translate-y-4 text-center text-base opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100">
                         {service.shortDescription}
                     </p>
                 </div>

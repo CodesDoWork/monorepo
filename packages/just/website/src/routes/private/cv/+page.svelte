1e334022b08d4255a7664303e9d6c5285b56dda0
Justin Konratt <codesdowork@gmail.com>
2025-04-27 09:11:42 +0200

    feat(just-website): cv background and access

--- packages/just/website/src/routes/private/cv/+page.svelte
+++ packages/just/website/src/routes/private/cv/+page.svelte
@@ -1,5 +1,16 @@
+<script lang="ts">
+    import { clsx } from "clsx";
+</script>
+
 <div class="min-w-screen min-h-screen flex justify-center items-center">
-    <div class="w-a4 h-a4 p-a4-page-padding shadow-a4 bg-white box-border">My CV</div>
+    <div
+        class={clsx(
+            "w-a4 h-a4 p-a4-page-padding shadow-a4 box-border",
+            "from-primary-400 to-secondary-400 dark:from-primary-950 dark:to-secondary-950",
+            "bg-gradient-to-b from-5% to-95% transition-colors",
+        )}>
+        My CV
+    </div>
 </div>
 
 <svelte:head>

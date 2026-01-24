dd05ba2a110fbba5a57ba1ffee2dbe1cf391073d
Justin Konratt <codesdowork@gmail.com>
2026-01-24 14:16:06 +0100

    fix(shared-svelte-components): fix virtual list scrolling for iOS

--- packages/shared/svelte/components/src/virtual-list/VirtualList.svelte
+++ packages/shared/svelte/components/src/virtual-list/VirtualList.svelte
@@ -13,22 +13,49 @@
 
     const { class: className, itemContainerClass, items, buffer = 3, children }: Props = $props();
     let outsideItems = $state<boolean[]>([]);
+    let elementHeights = $state<number[]>([]);
 
     $effect(() => {
         outsideItems = items.map((_, idx) => idx !== 0);
+        elementHeights = items.map(() => 0);
     });
 
     const offset = $derived(Math.max(0, outsideItems.indexOf(false) - buffer));
     const end = $derived(outsideItems.lastIndexOf(false) + buffer + 1);
-    const displayedItems = $derived(items.slice(offset, end));
+    const displayedItems = $derived(items.slice(offset, end + buffer));
+
+    const paddingTop = $derived(elementHeights.slice(0, offset).reduce((a, b) => a + b, 0));
+
+    function resizeAction(node: HTMLDivElement, callback: (height: number) => void) {
+        const resizeObserver = new ResizeObserver(() => {
+            if (node && node.clientHeight) {
+                callback(node.clientHeight);
+            }
+        });
+        resizeObserver.observe(node);
+
+        return {
+            destroy() {
+                resizeObserver.disconnect();
+            },
+        };
+    }
 </script>
 
-<ul class={clsx(className, "scrollbar-hide size-full min-h-0 overflow-y-auto")}>
+<ul class={clsx(className, "scrollbar-hide relative size-full min-h-0 overflow-y-auto")}>
+    <div style="padding-top: {paddingTop}px"></div>
     {#each displayedItems as item, idx (idx + offset)}
         {@const globalIdx = idx + offset}
         <div
             class={itemContainerClass}
-            use:overflowOberserver={info => (outsideItems[globalIdx] = info.outside.parent)}>
+            use:resizeAction={height => (elementHeights[globalIdx] = height)}
+            use:overflowOberserver={info => {
+                requestAnimationFrame(() => {
+                    outsideItems = outsideItems.map((v, i) =>
+                        i === globalIdx ? info.outside.parent : v,
+                    );
+                });
+            }}>
             {@render children(item, globalIdx)}
         </div>
     {/each}

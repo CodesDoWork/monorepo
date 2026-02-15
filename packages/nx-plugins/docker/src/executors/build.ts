f27ef9c8269f854e6fc100b6049d2be2e8a40592
Justin Konratt <codesdowork@gmail.com>
2026-02-15 19:48:02 +0100

    feat(nx-plugins-docker): add caching sources only if manifest exists

--- packages/nx-plugins/docker/src/executors/build.ts
+++ packages/nx-plugins/docker/src/executors/build.ts
@@ -1,22 +1,29 @@
 import type { PromiseExecutor } from "@nx/devkit";
 import type { ExecutorSchema } from "./schema";
 import { projectRoot } from "@cdw/monorepo/nx-plugins-utils";
+import { execAsync } from "@cdw/monorepo/packages/shared/utils/src";
 import { dockerImage, getBaseDockerVars, runDockerCommand } from "../utils";
 
 export const dockerBuildExecutor: PromiseExecutor<ExecutorSchema> = async ({ args }, context) => {
     try {
         const { IMAGE_BASE, PROJECT_VERSION, DOCKER_PROXY, CI } = getBaseDockerVars();
         const image = dockerImage(context.projectName ?? "");
-        const cacheImage = dockerImage(`${context.projectName}-cache`);
+        const cacheImageName = `${context.projectName}-cache`;
+        const cacheImage = dockerImage(cacheImageName);
         const platform = CI ? "linux/arm64" : "";
         const ciOptions = CI
-            ? [
-                  "--push",
-                  "--network=host",
-                  `--cache-to type=registry,ref=${cacheImage},mode=max`,
-                  `--cache-from type=registry,ref=${cacheImage}`,
-              ]
+            ? ["--push", "--network=host", `--cache-to type=registry,ref=${cacheImage},mode=max`]
             : ["--load"];
+
+        if (CI) {
+            const devCacheImage = dockerImage(cacheImageName, "develop");
+            await execAsync("docker", ["manifest", "inspect", devCacheImage]).then(() =>
+                ciOptions.push(`--cache-from type=registry,ref=${devCacheImage}`),
+            );
+            await execAsync("docker", ["manifest", "inspect", cacheImage]).then(() =>
+                ciOptions.push(`--cache-from type=registry,ref=${cacheImage}`),
+            );
+        }
 
         const latestImageIfNeeded =
             CI &&

3bc31485e98daac79eaeaa453fc6a67a78bd1670
Justin Konratt <codesdowork@gmail.com>
2026-02-20 09:01:15 +0100

    chore(research): remove old python project and python at all from the repo

--- shell.nix
+++ shell.nix
@@ -6,9 +6,6 @@ pkgs.mkShell {
   packages = with pkgs; [
     nodejs_25
     bun
-    python313
-    python313Packages.pip
-    python313Packages.uv
     texlive.combined.scheme-full # LaTeX
     ltex-ls-plus # LaTeX
     ffmpeg_7-headless # audio tool (just-music-store, just-music-marketplace)

63f84365da443ed9f2b258e3971deee7028503d2
Justin Konratt <codesdowork@gmail.com>
2026-02-21 16:05:36 +0100

    ci: fix trivy and add trivy reports

---
+++ packages/shared/utils/src/files.ts
@@ -0,0 +1,14 @@
+import { existsSync } from "node:fs";
+import { dirname, join } from "node:path";
+
+export function findNextHigherDirWith(filename: string): string {
+    let dir = process.cwd();
+    while (!existsSync(join(dir, filename))) {
+        if (dir === "/") {
+            throw new Error(`Could not find ${filename}`);
+        }
+
+        dir = dirname(dir);
+    }
+    return dir;
+}
